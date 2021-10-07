3839fcc9b8202509656fbc6ea4c0fad2c98474ab
Kgolagano <kmoetle@gmail.com>
2021-10-07 23:18:48 +0200

    MaintainRespondents V2

--- MaintainRespondents/Form1.Designer.cs
+++ MaintainRespondents/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.Respondents = new System.Windows.Forms.GroupBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.txtStrName = new System.Windows.Forms.TextBox();
             this.txtStrNo = new System.Windows.Forms.TextBox();
@@ -39,19 +43,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRespID = new System.Windows.Forms.TextBox();
             this.Respondents.SuspendLayout();
             this.SuspendLayout();
             // 
             // Respondents
             // 
-            this.Respondents.Controls.Add(this.textBox1);
+            this.Respondents.Controls.Add(this.txtRespID);
+            this.Respondents.Controls.Add(this.label7);
+            this.Respondents.Controls.Add(this.txtCity);
             this.Respondents.Controls.Add(this.label6);
             this.Respondents.Controls.Add(this.dateTimePicker1);
-            this.Respondents.Controls.Add(this.button1);
+            this.Respondents.Controls.Add(this.btnDelete);
             this.Respondents.Controls.Add(this.btnSubmit);
             this.Respondents.Controls.Add(this.txtStrName);
             this.Respondents.Controls.Add(this.txtStrNo);
@@ -64,14 +68,49 @@
             this.Respondents.Controls.Add(this.label1);
             this.Respondents.Location = new System.Drawing.Point(13, 13);
             this.Respondents.Name = "Respondents";
-            this.Respondents.Size = new System.Drawing.Size(482, 311);
+            this.Respondents.Size = new System.Drawing.Size(534, 373);
             this.Respondents.TabIndex = 0;
             this.Respondents.TabStop = false;
             this.Respondents.Text = "Maintain Respondents";
             // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(228, 236);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(219, 20);
+            this.txtCity.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "City :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(228, 128);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(248, 288);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(122, 28);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button1_Click);
+            // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(58, 266);
+            this.btnSubmit.Location = new System.Drawing.Point(69, 288);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(122, 28);
             this.btnSubmit.TabIndex = 10;
@@ -81,28 +120,28 @@
             // 
             // txtStrName
             // 
-            this.txtStrName.Location = new System.Drawing.Point(217, 179);
+            this.txtStrName.Location = new System.Drawing.Point(228, 201);
             this.txtStrName.Name = "txtStrName";
             this.txtStrName.Size = new System.Drawing.Size(219, 20);
             this.txtStrName.TabIndex = 9;
             // 
             // txtStrNo
             // 
-            this.txtStrNo.Location = new System.Drawing.Point(217, 142);
+            this.txtStrNo.Location = new System.Drawing.Point(228, 164);
             this.txtStrNo.Name = "txtStrNo";
             this.txtStrNo.Size = new System.Drawing.Size(219, 20);
             this.txtStrNo.TabIndex = 8;
             // 
             // txtSurname
             // 
-            this.txtSurname.Location = new System.Drawing.Point(217, 71);
+            this.txtSurname.Location = new System.Drawing.Point(228, 93);
             this.txtSurname.Name = "txtSurname";
             this.txtSurname.Size = new System.Drawing.Size(219, 20);
             this.txtSurname.TabIndex = 6;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(217, 42);
+            this.txtName.Location = new System.Drawing.Point(228, 64);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(219, 20);
             this.txtName.TabIndex = 5;
@@ -110,7 +149,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 186);
+            this.label5.Location = new System.Drawing.Point(18, 208);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(72, 13);
             this.label5.TabIndex = 4;
@@ -119,7 +158,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 149);
+            this.label4.Location = new System.Drawing.Point(18, 171);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 13);
             this.label4.TabIndex = 3;
@@ -128,7 +167,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 112);
+            this.label3.Location = new System.Drawing.Point(17, 134);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 2;
@@ -137,7 +176,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 78);
+            this.label2.Location = new System.Drawing.Point(18, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 1;
@@ -146,51 +185,33 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 49);
+            this.label1.Location = new System.Drawing.Point(17, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Name :";
             // 
-            // button1
+            // label7
             // 
-            this.button1.Location = new System.Drawing.Point(237, 266);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 28);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Delete";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Respondent ID:";
             // 
-            // dateTimePicker1
+            // txtRespID
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(217, 106);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(132, 20);
-            this.dateTimePicker1.TabIndex = 12;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 217);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(30, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "City :";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(217, 214);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(219, 20);
-            this.textBox1.TabIndex = 14;
+            this.txtRespID.Location = new System.Drawing.Point(228, 37);
+            this.txtRespID.Name = "txtRespID";
+            this.txtRespID.Size = new System.Drawing.Size(219, 20);
+            this.txtRespID.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 362);
+            this.ClientSize = new System.Drawing.Size(600, 425);
             this.Controls.Add(this.Respondents);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -214,9 +235,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtCity;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRespID;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- MaintainRespondents/Form1.cs
+++ MaintainRespondents/Form1.cs
@@ -22,11 +22,13 @@ namespace MaintainCities
         public SqlDataAdapter adap;
         DataSet ds;
         SqlDataReader theReader;
+        public int fRespID;
         public string fName; //Declaring variable with access modifier publicly
         public string fSurname;
         public string fStrName;
         public int fDOB;
         public int fStrNo;
+        public int fCityID;
 
 
 
@@ -42,23 +44,27 @@ namespace MaintainCities
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            fRespID = Convert.ToInt32(txtRespID.Text);
             fName = txtName.Text;
             fSurname = txtSurname.Text;
             //fDOB = Convert.ToInt32(txtDOB.Text);
             fStrNo = Convert.ToInt32(txtStrNo.Text);
-            fStrName = txtStrName.Text;           
+            fStrName = txtStrName.Text;
+            fCityID = Convert.ToInt32(txtCity.Text);
                         
             try
             {
                 SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-PA4SGPS2\SQLEXPRESS;Initial Catalog = spCreateTables; Integrated Security = True"); //Connection to connect and update the database with the Respondents details
                 conn.Open();
-                SqlCommand SqlInsert = new SqlCommand($"INSERT INTO Respondent  VALUES ('"+txtName.Text +"', '"+txtSurname.Text + "','" + this.dateTimePicker1.Text + "','" + Convert.ToInt32(txtStrNo.Text) + "','" + txtStrName.Text + "')", conn); 
+                SqlCommand SqlInsert = new SqlCommand($"INSERT INTO Respondent (RespID,Name, Surname, DOB, StreetNumber, StreetName,City)  VALUES ('"+Convert.ToInt32(txtRespID.Text)+ "', '"+txtName.Text +"', '"+txtSurname.Text + "','" + this.dateTimePicker1.Text + "','" + Convert.ToInt32(txtStrNo.Text) + "','" + txtStrName.Text + "', '" + Convert.ToInt32(txtCity.Text) + "') AND ", conn); 
                 SqlDataAdapter adap = new SqlDataAdapter();
                 adap.InsertCommand = SqlInsert;
                 adap.InsertCommand.ExecuteNonQuery();
+                SqlCommand SqlCity = new SqlCommand("INSERT INTO City (CityID) VALUES ('" + Convert.ToInt32(txtCity.Text) + "");
 
                 conn.Close();
-
+                MessageBox.Show("Respondent Successfully Added and updated");
+                
             }
             catch (SqlException error)
             {
@@ -74,6 +80,27 @@ namespace MaintainCities
         private void label6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                //Connection to connect and Delete the respondents data on the databse 
+                conn.Open();
+                string sql = "DELETE FROM Respondent WHERE RespID = Convert.ToInt32(txtRespID.Text)" ;
+                SqlCommand comm = new SqlCommand(sql, conn);
+                SqlDataReader theReader = comm.ExecuteReader();
+
+                conn.Close();
+                MessageBox.Show("Respondent Successfully Deleted");
+
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
         }
     }
 }
