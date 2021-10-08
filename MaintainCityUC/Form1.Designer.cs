964d8083b5b527aa093186b1561457c7181ef5a4
Kgolagano <kmoetle@gmail.com>
2021-10-08 20:56:04 +0200

    MaintaincityUC V1

---
+++ MaintainCityUC/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MaintainCityUC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCityID = new System.Windows.Forms.TextBox();
+            this.txtCityName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtCityName);
+            this.groupBox1.Controls.Add(this.txtCityID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 231);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Maintain City";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City ID  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "City Name :";
+            // 
+            // txtCityID
+            // 
+            this.txtCityID.Location = new System.Drawing.Point(169, 49);
+            this.txtCityID.Name = "txtCityID";
+            this.txtCityID.Size = new System.Drawing.Size(145, 20);
+            this.txtCityID.TabIndex = 2;
+            // 
+            // txtCityName
+            // 
+            this.txtCityName.Location = new System.Drawing.Point(169, 103);
+            this.txtCityName.Name = "txtCityName";
+            this.txtCityName.Size = new System.Drawing.Size(145, 20);
+            this.txtCityName.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(45, 162);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(107, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add /Update";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(195, 162);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(107, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 286);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtCityName;
+        private System.Windows.Forms.TextBox txtCityID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MaintainCityUC/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MaintainCityUC
+{
+    public partial class Form1 : Form
+    {
+
+        public SqlConnection conn;
+
+        //Declaring the public variables
+        String conStr = @"Data Source=LAPTOP-PA4SGPS2\SQLEXPRESS;Initial Catalog = spCreateTables; Integrated Security = True";
+        //SqlConnection conn;
+        SqlCommand comm;
+        public SqlDataAdapter adap;
+        DataSet ds;
+        SqlDataReader theReader;
+        public int fCityID;
+        public string fCityName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            fCityID = Convert.ToInt32(txtCityID.Text);
+            fCityName = txtCityName.Text;
+        try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-PA4SGPS2\SQLEXPRESS;Initial Catalog = spCreateTables; Integrated Security = True"); //Connection to connect and update the database with the Respondents details
+                conn.Open();
+                SqlCommand SqlInsert = new SqlCommand($"INSERT INTO City (CityID, CityName)  VALUES ('" + Convert.ToInt32(txtCityID.Text) + "', '" + txtCityName.Text + "',)", conn);
+                SqlDataAdapter adap = new SqlDataAdapter();
+                adap.InsertCommand = SqlInsert;
+                adap.InsertCommand.ExecuteNonQuery();
+
+                conn.Close();
+                MessageBox.Show("City Successfully Added and updated");
+            }
+        
+        
+        
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+}
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Connection to connect and Delete the respondents data on the databse 
+                conn.Open();
+                string sql = "DELETE FROM City WHERE CityID = Convert.ToInt32(txt.CityID.Text)";
+                SqlCommand comm = new SqlCommand(sql, conn);
+                SqlDataReader theReader = comm.ExecuteReader();
+
+                conn.Close();
+                MessageBox.Show("City Successfully Deleted");
+
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
